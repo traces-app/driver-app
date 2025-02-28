7a1dfa8aa372e6f3f35111a03ed2b359eaa3d33b
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-02-28 23:08:04 +0530

    authenticated a single user on app startup for future use cases, testing purposes only

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,89 @@ import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Map View'),
+      debugShowCheckedModeBanner: false,
+      home: AuthCheckScreen(), // Check authentication before showing the map
     );
+  }
+}
+
+class AuthCheckScreen extends StatefulWidget {
+  @override
+  _AuthCheckScreenState createState() => _AuthCheckScreenState();
+}
+
+class _AuthCheckScreenState extends State<AuthCheckScreen> {
+  bool isAuthenticated = false;
+  String? errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    _signIn();
+  }
+
+  Future<void> _signIn() async {
+    try {
+      // TODO: handle authentication properly for different user entered accounts
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+            email: "testuser@example.com",
+            password: "testpassword",
+          );
+
+      setState(() {
+        isAuthenticated = true;
+      });
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        setState(() {
+          errorMessage = 'No user found for that email.';
+        });
+      } else if (e.code == 'wrong-password') {
+        setState(() {
+          errorMessage = 'Wrong password provided for that user.';
+        });
+      } else {
+        setState(() {
+          errorMessage = e.message;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isAuthenticated) {
+      return const MyHomePage(title: 'Map View');
+    } else {
+      return Scaffold(
+        appBar: AppBar(title: const Text("Logging In...")),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Center(
+            child:
+                errorMessage == null
+                    ? const CircularProgressIndicator()
+                    : Text("Error: $errorMessage"),
+          ),
+        ),
+      );
+    }
   }
 }
 
@@ -41,24 +106,35 @@ class _MyHomePageState extends State<MyHomePage> {
   void _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
 
-    // load json from assets
+    // Load JSON from assets
     String styleJson = await rootBundle.loadString('assets/map/styles.json');
 
-    // apply styles to the google map
+    // Apply styles to the Google Map
     mapController.setMapStyle(styleJson);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.black,
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
         elevation: 2,
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 15.0,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
